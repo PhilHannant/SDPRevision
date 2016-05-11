cd367cfe77e0a1f24eb2aa995bd10ad6a753f364
philhannant <philip.a.hannant@gmail.com>
2016-05-11 16:47:47 +0100

    tricky

--- src/wk8/IntList.scala
+++ src/wk8/IntList.scala
@@ -1,31 +1,26 @@
 package wk8
 
-sealed trait IntList { def length: Int =
-  this match {
-    case End => 0
-    case Pair(hd, tl) => 1 + tl.length
-  }
-  def double: IntList =
-    this match {
-      case End => End
-      case Pair(hd, tl) => Pair(hd * 2, tl.double)
-    }
-  def product: Int =
-    this match {
-      case End => 1
-      case Pair(hd, tl) => hd * tl.product
-    }
-  def sum: Int =
-    this match {
-      case End => 0
-      case Pair(hd, tl) => hd + tl.sum
-    }
+sealed trait IntList {
 
-  def fold(end: Int, f: (Int, Int) => Int): Int =
+  def fold[T](end: T, f: (Int, T) => T): T =
     this match {
       case End => end
       case Pair(hd, tl) => f(hd, tl.fold(end, f))
     }
+  def length: Int =
+    fold(0, (_, tl) =>  1 + tl)
+
+
+  def product: Int =
+    fold(0, (hd, tl) => hd * tl)
+
+  def sum: Int =
+    fold(0, (hd, tl) => hd + tl)
+
+  def double: IntList =
+    fold[IntList](End, (hd, tl) => Pair(hd * 2, tl.double))
+
+
 }
 final case object End extends IntList
 final case class Pair(head: Int, tail: IntList) extends IntList
